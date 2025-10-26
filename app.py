a9ac4b854170427bf42901f1eda6989670239d30
Lubomur <luba81816@gmail.com>
2025-10-26 11:03:15 +0200

    Add guides page and route

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/guides')
+def guides():
+    return render_template('guides.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
